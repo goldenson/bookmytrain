786e902ecdb244bfb6f60760a8395b0e0f7e9b40
Goldenson <orefice.maxime@gmail.com>
2017-11-01 22:46:25 -0400

    add sigin and pick the best ticket - flow completed

--- app/models/bot_booking.rb
+++ app/models/bot_booking.rb
@@ -1,19 +1,81 @@
 class BotBooking
+  PURCHASED_ATTEMPTS = 2
+
   def initialize
-    @reservation = TrainBooking.first
+    @reservation = TrainBooking.last
     @trainline_url = "https://www.trainline.fr/"
     @browser = Capybara.current_session
     @driver = @browser.driver.browser
+    @countdown = 0
+  end
+
+  def flow
+    sign_in_as_max
+    search_for_results
+    pick_the_best_result
+    checkout
+  end
+
+  def sign_in_as_max
+    visit_trainline
+    sleep 1
+    @browser.find('.header__signin-button').click
+    sleep 1
+    @browser.find('#signin-form input[type=email]').set('orefice.maxime@gmail.com')
+    sleep 1
+    @browser.find('#signin-form input[type=password]').set('131006')
+    sleep 1
+    @browser.find('.signin__button').click
   end
 
   def search_for_results
-    visit_trainline
     fill_in_city_departure
     fill_in_city_arrival
     pick_right_month
     pick_right_datetime_departure
-    pick_right_datetime_arrival
+    # pick_right_datetime_arrival
     submit_search
+  end
+
+  def pick_the_best_result
+    sleep 1
+    @browser.all('.search__results--table .search__results--line .first span.time').each_with_index do |node, index|
+      @browser.execute_script("$($('.search__results--line')[#{index}]).css('background','pink')")
+      puts "NODE #{index} for #{node.text}"
+      if node.text[0..4].to_i >= @reservation.time_departure.hour
+        puts "NODE MATCH #{index} for #{node.text}"
+        if @browser.all('.search__results--table .search__results--line .third')[index]['class'].include? 'unsellable'
+          puts "NODE UNAVAILABLE #{index} for #{node.text} count #{@countdown}"
+          if @countdown >= PURCHASED_ATTEMPTS || @countdown < 0
+            @countdown = -1
+            next
+          end
+          @countdown += 1 if @countdown >= 0
+          # sleep 60
+          sleep 1
+          @driver.navigate.refresh
+          sleep 2
+          submit_search
+          sleep 1
+          pick_the_best_result
+          break
+        end
+        puts "NODE CLICKED #{index} for #{node.text}"
+        node.click
+        break
+      end
+    end
+  end
+
+  def checkout
+    sleep 6
+    # add to cart
+    @browser.find('.selected-folder__button button').click
+    sleep 6
+    pick_the_best_result unless @browser.current_path == '/cart'
+
+    # buy ticket
+    # @browser.find('cart__group').click
   end
 
   def visit_trainline
@@ -34,8 +96,8 @@ class BotBooking
 
   def pick_right_month
     delta_mat = @reservation.date_departure.month - @browser.find('.search__calendar--current-month').text.to_datetime.month
-    delta_mat = 12 + delta_mat if delta_mat.negative? # new year case
-    
+    delta_mat = 12 + delta_mat if delta_mat.negative? # next year case
+
     delta_mat.times do
       @browser.find('.search__calendar--increment-month').click
       sleep 1
@@ -78,7 +140,7 @@ class BotBooking
         break
       end
     end
-  end
 
-  sleep 1
+    sleep 1
+  end
 end
--- lib/tasks/automated_train_booking.rake
+++ lib/tasks/automated_train_booking.rake
@@ -1,6 +1,6 @@
 desc 'Book a train for Millie on trainline.fr'
 task automated_train_booking: :environment do
-  automated_reservation = BotBooking.new
-  automated_reservation.search_for_results
-  sleep 10
+  reservation = BotBooking.new
+  reservation.flow
+  sleep 1
 end
