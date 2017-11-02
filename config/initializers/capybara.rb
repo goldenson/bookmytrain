d54bf19e454601e66ed45d390d3e48324aae26ac
Goldenson <orefice.maxime@gmail.com>
2017-11-01 23:20:52 -0400

    add capybary to heroku againn

--- config/initializers/capybara.rb
+++ config/initializers/capybara.rb
@@ -1,11 +1,16 @@
 chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
-
 chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
 
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(
-     app,
-     browser: :chrome,
-     desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
-  )
+Capybara.register_driver :selenium do |app|
+  if chrome_bin
+    Capybara::Selenium::Driver.new(
+      app,
+      browser: :chrome,
+      desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
+    )
+  else
+    Capybara::Selenium::Driver.new(app, browser: :chrome)
+  end
 end
+
+Capybara.default_driver = :selenium
