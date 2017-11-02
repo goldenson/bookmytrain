65542603dc6c59f37a4c7c9dbaaad78f14f881dc
Goldenson <orefice.maxime@gmail.com>
2017-11-01 23:17:16 -0400

    add capybary to heroku again

--- config/initializers/capybara.rb
+++ config/initializers/capybara.rb
@@ -1,9 +1,11 @@
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
-end
+chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
 
-Capybara.javascript_driver = :chrome
-Capybara.configure do |config|
-  config.default_max_wait_time = 10 # seconds
-  config.default_driver = :selenium
+chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
+
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(
+     app,
+     browser: :chrome,
+     desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
+  )
 end
