376bb4b40f6a7d531e772264f7258fa6777396fa
Goldenson <orefice.maxime@gmail.com>
2017-11-02 23:13:44 -0400

    set up capybary for digital ocean

--- config/initializers/capybara.rb
+++ config/initializers/capybara.rb
@@ -1,14 +1,9 @@
-chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
-
-options = {}
-options[:args] = ['headless', 'disable-gpu', 'window-size=1280,1024']
-options[:binary] = chrome_bin if chrome_bin
-
-Capybara.register_driver :headless_chrome do |app|
-  Capybara::Selenium::Driver.new(app,
-     browser: :chrome,
-     options: Selenium::WebDriver::Chrome::Options.new(options)
-   )
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
 
-Capybara.javascript_driver = :headless_chrome
+Capybara.javascript_driver = :chrome
+Capybara.configure do |config|
+  config.default_max_wait_time = 10 # seconds
+  config.default_driver = :selenium
+end
