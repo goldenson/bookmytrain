e8a14460190220d65f9c7716d1739b4a2861d27b
Goldenson <orefice.maxime@gmail.com>
2017-11-03 18:20:42 -0400

    set up capybara for production

--- config/initializers/capybara.rb
+++ config/initializers/capybara.rb
@@ -2,6 +2,7 @@ Capybara.register_driver :selenium do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
 
+Capybara.javascript_driver = :chrome
 Capybara.configure do |config|
   config.default_max_wait_time = 10.seconds
   config.default_driver = :selenium
