e7f243bc7a1080826cb74be176afca4efd47dfb2
Goldenson <orefice.maxime@gmail.com>
2017-11-03 18:30:44 -0400

    set up capybara for production

--- config/initializers/capybara.rb
+++ config/initializers/capybara.rb
@@ -1,9 +1,17 @@
-Capybara.register_driver :selenium do |app|
+require "selenium/webdriver"
+
+Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
 
-Capybara.javascript_driver = :chrome
-Capybara.configure do |config|
-  config.default_max_wait_time = 10.seconds
-  config.default_driver = :selenium
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w(headless disable-gpu) }
+  )
+
+  Capybara::Selenium::Driver.new app,
+    browser: :chrome,
+    desired_capabilities: capabilities
 end
+
+Capybara.javascript_driver = :headless_chrome
