6109013e1114157a60989adde029fcc0aa5bb1ab
Goldenson <orefice.maxime@gmail.com>
2017-11-03 17:57:42 -0400

    set up capybara for production

--- config/initializers/capybara.rb
+++ config/initializers/capybara.rb
@@ -7,3 +7,6 @@ Capybara.configure do |config|
   config.default_max_wait_time = 10 # seconds
   config.default_driver = :selenium
 end
+
+Capybara.app_host = ENV.fetch("TESTED_HOST")
+Capybara.run_server = false
