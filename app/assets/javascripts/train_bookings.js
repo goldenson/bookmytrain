4d08af557328cbf7685602c1bb358c9f3645fc33
Goldenson <orefice.maxime@gmail.com>
2017-11-05 11:10:16 -0500

    add city autocompletion

--- app/assets/javascripts/train_bookings.js
+++ app/assets/javascripts/train_bookings.js
@@ -1,2 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+document.addEventListener("turbolinks:load", function() {
+  const city_departure = places({
+    container: document.querySelector('#train_booking_city_departure'),
+    countries: 'fr',
+  });
+
+  const city_arrival = places( {
+    container: document.querySelector('#train_booking_city_arrival'),
+    countries: 'fr',
+  });
+});
--- app/models/train_booking.rb
+++ app/models/train_booking.rb
@@ -3,4 +3,13 @@ class TrainBooking < ApplicationRecord
   validates :city_arrival, presence: true
   validates :date_departure, presence: true
   validates :time_departure, presence: true
+
+  before_save :sanitize_cities
+
+  private
+
+  def sanitize_cities
+    self.city_departure = self.city_departure.split(',').first
+    self.city_arrival = self.city_arrival.split(',').first
+  end
 end
