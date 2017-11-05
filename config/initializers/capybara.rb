c3149f29458b968a1554d46e9f44264ac016d6e7
Goldenson <orefice.maxime@gmail.com>
2017-11-05 14:50:09 -0500

    add capybara config for development

--- config/initializers/capybara.rb
+++ config/initializers/capybara.rb
@@ -1,18 +1,30 @@
-Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w[headless disable-gpu] }
-  )
+if Rails.env.production?
+  Capybara.register_driver :headless_chrome do |app|
+    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+      chromeOptions: { args: %w[headless disable-gpu] }
+    )
 
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :chrome,
-    desired_capabilities: capabilities
-  )
-end
+    Capybara::Selenium::Driver.new(
+      app,
+      browser: :chrome,
+      desired_capabilities: capabilities
+    )
+  end
 
-Capybara.javascript_driver = :headless_chrome
+  Capybara.javascript_driver = :headless_chrome
 
-Capybara.configure do |config|
-  config.default_max_wait_time = 10.seconds
-  config.default_driver = :headless_chrome
+  Capybara.configure do |config|
+    config.default_max_wait_time = 10.seconds
+    config.default_driver = :headless_chrome
+  end
+elsif Rails.env.development?
+  Capybara.register_driver :selenium do |app|
+    Capybara::Selenium::Driver.new(app, browser: :chrome)
+  end
+
+  Capybara.javascript_driver = :chrome
+  Capybara.configure do |config|
+    config.default_max_wait_time = 10 # seconds
+    config.default_driver = :selenium
+  end
 end
