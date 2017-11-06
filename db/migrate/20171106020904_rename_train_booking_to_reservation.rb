fc7864d0d1654e8698e44240be597e08f9ef99c2
Goldenson <orefice.maxime@gmail.com>
2017-11-05 21:44:34 -0500

    fix broken migration

--- db/migrate/20171106020904_rename_train_booking_to_reservation.rb
+++ db/migrate/20171106020904_rename_train_booking_to_reservation.rb
@@ -1,5 +1,5 @@
 class RenameTrainBookingToReservation < ActiveRecord::Migration[5.1]
   def change
-    rename_table 'reservations', 'reservations'
+    rename_table 'train_bookings', 'reservations'
   end
 end
