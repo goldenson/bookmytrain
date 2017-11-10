37a680eba6d3f6b17471c4b2e8e96f88260a3552
Goldenson <orefice.maxime@gmail.com>
2017-11-09 22:37:12 -0500

    add more set up to admin controller

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,9 +1,15 @@
 module Admin
   class ApplicationController < Administrate::ApplicationController
     before_action :authenticate_admin
+    before_action :default_params
 
     def authenticate_admin
-      redirect_to root_path unless current_user.admin?
+      redirect_to root_path, alert: 'Not authorized.' unless user_signed_in? && current_user.admin?
+    end
+
+    def default_params
+      params[:order] ||= "created_at"
+      params[:direction] ||= "desc"
     end
   end
 end
