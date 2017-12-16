41e23836bb152fcdfcf6975a90e9b64bedb179e0
Goldenson <maxime.orefice@shopify.com>
2017-12-16 21:51:46 +0100

    fix test on prod

--- test/controllers/reservations_controller_test.rb
+++ test/controllers/reservations_controller_test.rb
@@ -1,8 +1,12 @@
 require 'test_helper'
 
 class ReservationsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
     @reservation = reservations(:one)
+    @user = @reservation.user
+    sign_in @user
   end
 
   test "should get index" do
