1095f759ab3805f2d5683c5aa3467b19044eb66e
Goldenson <maxime.orefice@shopify.com>
2017-12-16 21:57:37 +0100

    fix travis on prod

--- test/system/reservations_test.rb
+++ test/system/reservations_test.rb
@@ -1,9 +1,16 @@
 require "application_system_test_case"
 
 class ReservationsTest < ApplicationSystemTestCase
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:max)
+    sign_in @user
+  end
+  
   test "visiting the index" do
     visit reservations_url
 
-    # assert_selector "h1", text: "Réservations"
+    assert_selector "h2", text: "Réservations"
   end
 end
