1b8282f59d78e42a1e540642e448f39ef61dfe35
Goldenson <maxime.orefice@shopify.com>
2017-12-20 16:57:57 +0100

    change success info color

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
       success: "alert-success",
       error: "alert-danger",
       alert: "alert-warning",
-      notice: "alert-info"
+      notice: "alert-success"
     }.stringify_keys[flash_type.to_s] || flash_type.to_s
   end
 
