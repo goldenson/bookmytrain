4db2334772e6ae3542c34389013f3afc09daf023
Goldenson <maxime.orefice@shopify.com>
2018-01-15 20:40:17 -0500

    remove auth with env var

--- config/initializers/sidekick.rb
+++ config/initializers/sidekick.rb
@@ -1,17 +1,3 @@
-require 'sidekiq/web'
-
-Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-  ActiveSupport::SecurityUtils.secure_compare(
-    ::Digest::SHA256.hexdigest(username),
-    ::Digest::SHA256.hexdigest(ENV["LOGIN"]
-    )
-  ) &
-  ActiveSupport::SecurityUtils.secure_compare(
-    ::Digest::SHA256.hexdigest(password),
-    ::Digest::SHA256.hexdigest(ENV["PASSWORD"]))
-end if Rails.env.production?
-
-
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_URL'] }
 end
